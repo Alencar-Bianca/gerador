001a74ed9db76864d3081b7656a2960d1159b8b3
Alencar-Bianca <alencar-bianca@hotmail.com>
2022-09-07 00:11:02 -0300

    projeto finalizado

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let slider = document.querySelector('#slider');
+let button = document.querySelector('#button');
+
+let sizePassword = document.querySelector('#valor');
+let password = document.querySelector('#password');
+let containerPassword = document.querySelector('#container-password');
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%¨&*()_';
+let novaSenha = '';
+
+sizePassword.innerHTML = slider.value
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword(){
+    let pass = '';
+    for(let i = 0, n = charset.length; i < slider.value; i++){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove('hide');
+    password.innerHTML = pass;
+}
+
+function copyPass(){
+    alert('Senha copiada com sucesso');
+    navigator.clipboard.writeText(novaSenha);
+}
